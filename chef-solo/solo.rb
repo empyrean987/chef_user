934dcb6263c0e9e74bec3e7d4b6ff41e39197c0e
Michael Knox <knoxm1@gmail.com>
2018-10-26 07:50:56 +0000

    updating chef_user and adding users cookbook

--- chef-solo/solo.rb
+++ chef-solo/solo.rb
@@ -1,3 +1,4 @@
 file_cache_path "/home/ubuntu/chef_user/chef-solo/"
 cookbook_path "/home/ubuntu/chef_user/cookbooks"
 json_attribs "/home/ubuntu/chef_user/chef-solo/node.json"
+data_bag_path "/home/ubuntu/chef_user/chef-solo/data_bags/"
